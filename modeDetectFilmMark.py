c34d9e1e0f5cd5a9ce99fcbd71c9be57dcf83040
DeadLekar <vladkovalenkoreg@mail.ru>
2018-07-13 00:23:48 +0300

    precise loading marks from db

--- modeDetectFilmMark.py
+++ modeDetectFilmMark.py
@@ -1,37 +1,40 @@
 import sqlite3 as lite
 import pandas as pd
+import serviceFunctions as sf
+from paths import *
 
-
-compName = 'notebook'
-driverPath = ""
-dbasePath = ""
-if compName == "Ilya":
-    driverPath = "C:/Users/илья/Dropbox/Ilya-Papa/father_files/drivers/chromedriver.exe"
-    dbasePath = "C:/tmp/kinoman.db"
-elif compName == "work":
-    driverPath = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe"
-    dbasePath = "C:/Users/vkovalenko/Dropbox/Ilya-Papa/father_files/bases/kinoman.db"
-elif compName == "notebook":
-    driverPath = "C:/Users/Vlad/Dropbox/Ilya-Papa/father_files/drivers/chromedriver.exe"
-    dbasePath = "C:/Users/Vlad/Dropbox/Ilya-Papa/father_files/bases/kinoman.db"
-
-conn = lite.connect(dbasePath)
-c = conn.cursor()
+db_path = sf.get_right_path(db_paths)
+conn_main = lite.connect(db_path + '/kinoman_imdb_main.db')
+conn1 = lite.connect(db_path + '/kinoman_imdb_marks_1.db')
+conn2 = lite.connect(db_path + '/kinoman_imdb_marks_2.db')
+c_main = conn_main.cursor()
+c1 = conn1.cursor()
+c2 = conn2.cursor()
 
 TARGET_FILM_ID = 1064  # DOCTOR STRANGE
 
 # get target film's marks and users
 target_dict = {}
-target_users_rows = c.execute('SELECT user_id, mark FROM marks WHERE film_id = {}'.format(str(TARGET_FILM_ID))).fetchall()
-for row in target_users_rows:
-    target_dict[row[0]] = [row[1], 0]
-df_target_film_marks = pd.DataFrame(target_dict)
+target_users = []
+for c in [c_main,c1,c2]:
+    target_users_rows = c.execute('SELECT user_id, mark FROM marks WHERE film_id = {}'.format(str(TARGET_FILM_ID))).fetchall()
+    for row in target_users_rows:
+        user_id = row[0]
+        mark = row[1]
+        target_dict[user_id] = [TARGET_FILM_ID, mark]
+        target_users.append(row[0])
+        print('{};{};{}'.format(user_id, TARGET_FILM_ID, mark))
+
 
 # get target users' films and marks
-target_dict.clear()
-target_users_films_rows = c.execute('SELECT id, user_id, film_id, mark FROM marks WHERE user_id IN (SELECT user_id FROM marks WHERE film_id = {})'.format(str(TARGET_FILM_ID))).fetchall()
-for row in target_users_films_rows:
-    target_dict[row[0]] = [row[1], row[2], row[3]]
+for c in [c_main,c1,c2]:
+    for user_id in target_users:
+        target_users_films_rows = c.execute('SELECT film_id, mark FROM marks WHERE user_id = {}'.format(user_id)).fetchall()
+        for row in target_users_films_rows:
+            film_id = row[0]
+            mark = row[1]
+            target_dict[user_id] = [film_id, mark]
+            print('{};{};{}'.format(user_id, film_id, mark))
 df_target_users_films = pd.DataFrame(target_dict)
 
 # group films by the number of marks
---
+++ paths.py
@@ -0,0 +1,2 @@
+db_paths = ['C:/Scripts/db/']
+driver_paths = ['C:/Users/илья/Dropbox/Ilya-Papa/father_files/drivers/chromedriver.exe', 'C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe', 'C:/Users/Vlad/Dropbox/Ilya-Papa/father_files/drivers/chromedriver.exe']
