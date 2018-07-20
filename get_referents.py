5199026a9a8bccfc15ec32eb0a164c11bb837c70
DeadLekar <vladkovalenkoreg@mail.ru>
2018-07-20 09:51:12 +0300

    new way to detect user's referents

--- get_referents.py
+++ get_referents.py
@@ -1,95 +1,82 @@
 import sqlite3 as lite
 from paths import *
 import serviceFunctions as sf
-import numpy
+import pandas as pd
+import datetime
 
 class User:
     id = -1
-    c = None
-    marks = {}
-    linked_users = []
+    c = None # db connection
+    df_my_marks = None # user's marks, pandas DataFrame
+    checked_users = []
     def __init__(self, _id, _c):
         self.id = _id
         self.c = _c
+        self._get_marks()
 
-    def get_marks(self):
-        m_rows = c.execute("SELECT mark, filmID FROM marks WHERE userID = {}".format(self.id)).fetchall()
-        for row in m_rows:
-            self.marks[row[1]] = row[0]
+    def _get_marks(self):
+        l_users_rows = c.execute("SELECT film_id,mark_bin FROM marks WHERE user_id = {}".format(self.id)).fetchall()
+        film_id_col = []
+        mark_col = []
+        d = {}
+        for row in l_users_rows:
+            film_id_col.append(row[0])
+            mark_col.append(row[1])
+            d = {'film_id':film_id_col, 'mark':mark_col}
 
-    def get_linked_users(self):
-        if len(self.marks) > 0:
-            for film_id in self.marks.keys():
-                f = Film(film_id,self.c)
-                f.get_users()
-                for u in f.users.keys():
-                    self.linked_users.append(u)
+        self.df_my_marks = pd.DataFrame(d)
+        self.df_my_marks.index = self.df_my_marks.film_id
+        # self.df_my_marks.drop(self.df_my_marks.film_id,inplace=True)
+        del self.df_my_marks['film_id']
 
-        
-
-
-class Film:
-    id = -1
-    c = None
-    users = {}
-    def __init__(self,_id,_c):
-        self.id = _id
-        self.c = _c
-
-    def get_users(self):
-        u_rows = c.execute("SELECT user_id,mark FROM marks WHERE film_id = {}".format(self.id)).fetchall()
-        for row in u_rows:
-            self.users[row[0]] = row[1]
-
-
+class UserMatrix:
+    matrix = {}
+    def __init__(self, user1_df, user2_df, min_intersect):
+        """
+        creates matrix from users data
+        :param user1: pandas.DataFrame
+        :param user2: pandas.DataFrame
+        """
+        df = user1_df.merge(user2_df, left_index = True, right_index = True)
+        if df.shape[0] >= min_intersect:
+            self.matrix['pp'] = df[(df.mark_x == 1) & (df.mark_y == 1)].shape[0]
+            self.matrix['pn'] = df[(df.mark_x == 1) & (df.mark_y == -1)].shape[0]
+            self.matrix['np'] = df[(df.mark_x == -1) & (df.mark_y == 1)].shape[0]
+            self.matrix['nn'] = df[(df.mark_x == -1) & (df.mark_y == -1)].shape[0]
+            self.matrix['total_sum'] = df.size/2
 
 
 db_path = sf.get_right_path(db_paths)
-conn = lite.connect(db_path)
+conn = lite.connect(db_path + '/kinoman_imdb_marks.db')
 c = conn.cursor()
-users = c.execute("SELECT id FROM users WHERE flgChecked = 0 ORDER BY id")
-for u_row in users.fetchall():
-    cr_user = User(u_row[0],c)
-    cr_user.get_marks()
-    cr_user.get_linked_users()
+users_rows = c.execute("SELECT user_id FROM grp_users WHERE is_checked = 0 ORDER BY user_id")
+users_id_list = []
 
+for u_row in users_rows.fetchall():
+    users_id_list.append(u_row[0])
 
+MIN_INTERSECT = 3 # minimum number of films seen by both users in a pair
+for i in range(0,len(users_id_list)):
+    now = datetime.datetime.now()
+    user = User(users_id_list[i],c)
+    print('+++ user {} +++'.format(user.id))
+    total_spect = 0
+    for film_id in user.df_my_marks.index:
+        spectators_rows = c.execute("SELECT user_id FROM marks WHERE film_id={} AND user_id > {}".format(film_id,user.id)).fetchall()
+        for row in spectators_rows:
+            total_spect += len(spectators_rows)
+            ref_user_id = row[0]
+            if ref_user_id not in user.checked_users:
+                ref_user = User(ref_user_id,c)
+                m = UserMatrix(user.df_my_marks, ref_user.df_my_marks, MIN_INTERSECT)
+                user.checked_users.append(ref_user.id)
+                if m.matrix:
+                    c.execute("INSERT INTO PAIRS(user1_id,user2_id,pp,pn,np,nn,total_sum) VALUES ({},{},{},{},{},{},{})".format(user.id,ref_user_id,m.matrix['pp'],m.matrix['pn'],m.matrix['np'],m.matrix['nn'],m.matrix['total_sum']))
 
-    cr_user_id = u_row[0]
-    cr_user_marks = {}
-    cr_user_marks_rows = c.execute("SELECT mark, filmID FROM marks WHERE userID = {}".format(cr_user_id)).fetchall()
-    for mark in cr_user_marks_rows:
-        cr_user_marks[cr_user_marks_rows[1]] = cr_user_marks_rows[0]
-
-
-
-    users_to_compare = c.execute("SELECT id FROM users WHERE id > " + next_user)
-    cnt_commit = 0
-    for u1_row in users_to_compare.fetchall():
-        user_compare_id = u1_row[0]
-        user_compare_films = c.execute("SELECT mark, filmID FROM marks WHERE userID = " + str(user_compare_id)).fetchall()
-        cnt_fits = 0
-        diff_sum = 0
-        mid_diff = 0
-        for cr_mark in cr_user_films:
-            mark_to_remove = 0
-            for compare_mark in user_compare_films:
-                if cr_mark[1] == compare_mark[1]:
-                    mark_to_remove = compare_mark
-                    cnt_fits += 1
-                    diff = abs(cr_mark[0] - compare_mark[0])
-                    diff_sum += diff
-                    break
-            if mark_to_remove:
-                user_compare_films.remove(mark_to_remove)
-        if cnt_fits > 0:
-            mid_diff = diff_sum / cnt_fits
-        common_share = int(100 * cnt_fits / (len(cr_user_films) + len(user_compare_films)))
-        c.execute("INSERT INTO pairs (user1ID, user2ID, distance, commonShare) VALUES ("+ str(cr_user_id) + "," + str(user_compare_id) + "," + str(mid_diff) + "," + str(common_share) +")")
-        #cnt_commit +=1
-        #if cnt_commit == 5000:
-        #    conn.commit()
-        #    cnt_commit = 0
-    c.execute("UPDATE users SET flgChecked = 1 WHERE id = " + str(cr_user_id))
+    then = datetime.datetime.now()
+    delta = then - now
+    print('{} to check the user {}'.format(delta.seconds, user.id))
+    print('{} spectators found'.format(total_spect))
+    c.execute("UPDATE grp_users SET is_checked = 1 WHERE user_id = {}".format(user.id))
     conn.commit()
-    # print(cr_user_id)
+
